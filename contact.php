e294c3023216d22897edc19a046dc63395ec29b9
Sunny <macret.jonathan@gmail.com>
2018-12-12 15:50:45 +0100

    Formulaire html

---
+++ contact.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejob2</title>
+</head>
+
+<body>
+    <h1></h1>
+
+        <form name="formulaire" method="post" action="mailto:yourdaddy@papa.com">
+            <br><input type="text"><br>
+            <br><input type="email"><br>
+            <br><input type="phone"><br><br>
+            <textarea id="form-message"></textarea><br>
+            <input type="checkbox"><br>
+            <input type="submit" value="">
+        </form>
+</body>
+</html>
