c08d5d7103da26a0db12936722109baade60db24
Ozzoxsaz <ozzoxsaz@gmail.com>
2018-12-13 08:49:50 +0100

    css form

--- contact.php
+++ contact.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Ejob2</title>
 </head>
 
 <body>
 
-    <nav>
+    <nav class="navbar">
         <a href="">2017</a>
         <a href="">2018</a>
         <a href="">2019</a>
@@ -17,14 +18,26 @@
     <h1>Formulaire</h1>
 
     <a href="">Revenir à la page précédente.</a>
-
-        <form name="formulaire" method="post" action="mailto:yourdaddy@papa.com">
-            <br>Nom<input type="text"><br>
-            <br>Email<input type="email"><br>
-            <br>Téléphone<input type="phone"><br><br>
-            <textarea id="form-message">Message</textarea><br>
-            Newsletter<input type="checkbox"><br>
+    <form name="formulaire" method="post" action="mailto:yourdaddy@papa.com">
+            <label>   <br>Nom : </label> <input class="" type="text">
+            <label>  <br>Email : </label><input type="email">
+            <label>  <br>Téléphone : </label><input type="phone">
+           <label> Destinataire :</label> <select name="subject">
+                <option value="">Arbouille Maxime</option>
+                <option value="">Bouchez Marc</option>
+                <option value="">Delcampe Aymeric</option>
+                <option value="">Descharles Vincent</option>
+                <option value="">Guerinot Théo</option>
+                <option value="">Jeannin Jessy</option>
+                <option value="">Macret Jonathan</option>
+                <option value="">Mouillard Rémi</option>
+                <option value="">Passeleu Thomas</option>
+                <option value="">Reisenleiter Bryen</option>
+                <option value="">Thomas Antoine</option>
+           </select>
+            <label>   Message :</label> <textarea id="form-message"> </textarea>
+            <label>  Newsletter</label><input type="checkbox">
+            
             <input type="submit" value="Envoyer">
-        </form>
 </body>
 </html>
