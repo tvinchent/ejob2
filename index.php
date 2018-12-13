8116359b6687ea9deafedc6f208d25979b643697
Marc Bouchez <theparaplux@gmail.com>
2018-12-13 09:18:58 +0100

    Remise au propre

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
 <body>
 
- <fieldset> 
+
 <div class="flex">
   
  <h1>Ejob2</h1>
@@ -29,32 +29,32 @@
   <a href="contact.php">Accéder au formulaire</a>  
 
   <ul>
-    <fieldset><li>Arbouille Maxime</li></fieldset>
-    <fieldset><li>Bouchez Marc</li></fieldset>
-    <fieldset><li>Delcampe Aymeric</li></fieldset>
+    <li>Arbouille Maxime</li>
+    <li>Bouchez Marc</li>
+    <li>Delcampe Aymeric</li>
   </ul>
   <br>
 
   <ul>
-    <fieldset><li>Descharles Vincent</li></fieldset>
-    <fieldset><li>Guerinot Théo</li></fieldset>
-    <fieldset><li>Jeannin Jessy</li></fieldset>
+    <li>Descharles Vincent</li>
+    <li>Guerinot Théo</li>
+    <li>Jeannin Jessy</li>
   </ul>
   <br>
 
   <ul>
-    <fieldset><li>Macret Jonathan</li></fieldset>
-    <fieldset><li>Mouillard Rémi</li></fieldset>
-    <fieldset><li>Passeleu Thomas</li></fieldset>
+    <li>Macret Jonathan</li>
+    <li>Mouillard Rémi</li>
+    <li>Passeleu Thomas</li>
   </ul>
   <br>
 
   <ul>
-    <fieldset><li>Reisenleiter Bryan</li></fieldset>
-    <fieldset><li>Thomas Antoine</li></fieldset>
+    <li>Reisenleiter Bryan</li>
+    <li>Thomas Antoine</li>
   </ul>
 
-   </fieldset>
+
 
 </body>
 
