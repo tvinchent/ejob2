0fcc59effe02c8721db220163a025e5b3e1e1ce0
Sunny <macret.jonathan@gmail.com>
2018-12-13 09:52:51 +0100

    Responsive 1er

--- contact.php
+++ contact.php
@@ -11,35 +11,39 @@
 
 <body>
 
-    <nav class="navbar">
-        <a href="">2017</a>
-        <a href="">2018</a>
-        <a href="">2019</a>
-    </nav>
-    
-    <h1>Formulaire</h1>
-
-    <a href="index.php">Revenir à la page précédente.</a>
-    <form name="formulaire" method="post" action="mailto:yourdaddy@papa.com">
-            <label>   <br>Nom : </label> <input class="" type="text">
-            <label>  <br>Email : </label><input type="email">
-            <label>  <br>Téléphone : </label><input type="phone">
-           <label> Destinataire :</label> <select name="subject">
-                <option value="">Arbouille Maxime</option>
-                <option value="">Bouchez Marc</option>
-                <option value="">Delcampe Aymeric</option>
-                <option value="">Descharles Vincent</option>
-                <option value="">Guerinot Théo</option>
-                <option value="">Jeannin Jessy</option>
-                <option value="">Macret Jonathan</option>
-                <option value="">Mouillard Rémi</option>
-                <option value="">Passeleu Thomas</option>
-                <option value="">Reisenleiter Bryen</option>
-                <option value="">Thomas Antoine</option>
-           </select>
-            <label>   Message :</label> <textarea id="form-message"> </textarea>
-            <label>  Newsletter</label><input type="checkbox">
-            
-            <input type="submit" value="Envoyer">
+    <div class="container">
+        <nav class="navbar">
+            <a href="">2017</a>
+            <a href="">2018</a>
+            <a href="">2019</a>
+        </nav>
+        
+        <h1>Formulaire</h1>
+        
+        <a href="index.php">Revenir à la page précédente.</a>
+        <form name="formulaire" method="post" action="mailto:yourdaddy@papa.com">
+                <label>   <br>Nom : </label> <input class="" type="text">
+                <label>  <br>Email : </label><input type="email">
+                <label>  <br>Téléphone : </label><input type="phone">
+               <label> Destinataire :</label> <select name="subject">
+                    <option value="">Arbouille Maxime</option>
+                    <option value="">Bouchez Marc</option>
+                    <option value="">Delcampe Aymeric</option>
+                    <option value="">Descharles Vincent</option>
+                    <option value="">Guerinot Théo</option>
+                    <option value="">Jeannin Jessy</option>
+                    <option value="">Macret Jonathan</option>
+                    <option value="">Mouillard Rémi</option>
+                    <option value="">Passeleu Thomas</option>
+                    <option value="">Reisenleiter Bryen</option>
+                    <option value="">Thomas Antoine</option>
+               </select>
+                <label>   Message :</label> <textarea id="form-message"> </textarea>
+                <label><input type="checkbox"> Newsletter</label>
+                
+                <input type="submit" value="Envoyer">
+                
+        </form>
+    </div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -13,46 +13,46 @@
 <body>
 
 
-<div class="flex">
-  <h1>Ejob2</h1>
-    <nav>
-      <a href="">2017</a>
-      <a href="">2018</a>
-      <a href="">2019</a>
-    </nav>
-</div>
-
-
-  
-  <a href="contact.php">Accéder au formulaire</a>  
-
-  <ul>
-    <li>Arbouille Maxime</li>
-    <li>Bouchez Marc</li>
-    <li>Delcampe Aymeric</li>
-  </ul>
-  <br>
-
-  <ul>
-    <li>Descharles Vincent</li>
-    <li>Guerinot Théo</li>
-    <li>Jeannin Jessy</li>
-  </ul>
-  <br>
-
-  <ul>
-    <li>Macret Jonathan</li>
-    <li>Mouillard Rémi</li>
-    <li>Passeleu Thomas</li>
-  </ul>
-  <br>
-
-  <ul>
-    <li>Reisenleiter Bryan</li>
-    <li>Thomas Antoine</li>
-  </ul>
-
-
+<div class="container"><div class="flex">
+      <h1>Liste étudiants</h1>
+        <nav>
+          <a href="">2017</a>
+          <a href="">2018</a>
+          <a href="">2019</a>
+        </nav>
+    </div>
+    
+    
+      
+      <a href="contact.php">Accéder au formulaire</a>  
+    
+      <ul>
+        <li>Arbouille Maxime</li>
+        <li>Bouchez Marc</li>
+        <li>Delcampe Aymeric</li>
+      </ul>
+      <br>
+    
+      <ul>
+        <li>Descharles Vincent</li>
+        <li>Guerinot Théo</li>
+        <li>Jeannin Jessy</li>
+      </ul>
+      <br>
+    
+      <ul>
+        <li>Macret Jonathan</li>
+        <li>Mouillard Rémi</li>
+        <li>Passeleu Thomas</li>
+      </ul>
+      <br>
+    
+      <ul>
+        <li>Reisenleiter Bryan</li>
+        <li>Thomas Antoine</li>
+      </ul>
+    
+    </div>
 
 </body>
 
