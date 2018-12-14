bef1077438cc3dc6133ccce6145a9fec045d93f8
Marc Bouchez <theparaplux@gmail.com>
2018-12-14 15:32:50 +0100

    less

--- index.php
+++ index.php
@@ -4,10 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-   <link rel="stylesheet" href="style.css">
-   <!-- <link rel="stylesheet" type="text/css" href="index.css"> -->
+   <link rel="stylesheet/less" href="m_b_style-less.less">
    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
-
+    <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/3.9.0/less.min.js" ></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
    <script src="https://cdn.rawgit.com/nnattawat/flip/master/dist/jquery.flip.min.js"></script>
    
@@ -45,8 +44,8 @@
             <li class='modal-toggle' name="<?= $eleve['name'] ?>" spe="<?= $eleve['specialisation']?>" id="<?= $eleve['id'] ?>"><?= $eleve['name'] ?></li>
             <div class="modal" id="toggle<?= $eleve['id'] ?>">
               <span class="hideModal" id="<?= $eleve['id'] ?>"><i class="fas fa-times"></i></span>
-              <span class="leftModal" id="<?= $eleve['id']+1 ?>"><i class="fas fa-chevron-left"></i></span>
-              <span class="rightModal" id="<?= $eleve['id']-1 ?>"><i class="fas fa-chevron-right"></i></span>
+              <span class="leftModal" idToHide="<?= $eleve['id'] ?>" idToToggle="<?= $eleve['id']-1 ?>"><i class="fas fa-chevron-left"></i></span>
+              <span class="rightModal" idToHide="<?= $eleve['id'] ?>" idToToggle="<?= $eleve['id'] + 1 ?>"><i class="fas fa-chevron-right"></i></span>
               <p>TEST <?= $eleve['name'] ?></p>
               <br>
               <img src="./images/cv.png" alt=""></div>
@@ -104,17 +103,38 @@
         </div>
 
       <!-- SCRIPT -->
+      
 <script>
 $(document).ready(function(){
+
   $('.modal-toggle').on('click', function(){
     idToToggle = $(this).attr('id')
-    $('#toggle'+idToToggle).css('display', 'block')
+    $('#toggle'+idToToggle).fadeIn()
   })
   $('.hideModal').on('click', function(){
     idToHide = $(this).attr('id')
-    $('#toggle'+idToToggle).css('display', 'none')
+    $('#toggle'+idToHide).fadeOut()
   })
-  
+
+  $('.leftModal').on('click', function(){
+    idToToggle = $(this).attr('idToToggle')
+    if(idToToggle == 0) {
+      idToToggle = 11
+    }
+    idToHide = $(this).attr('idToHide')
+    $('#toggle'+idToToggle).css('display', 'block')
+    $('#toggle'+ idToHide).css('display', 'none')
+  })
+  $('.rightModal').on('click', function(){
+    idToToggle = $(this).attr('idToToggle')
+    if(idToToggle == 12) {
+      idToToggle = 1
+    }
+    idToHide = $(this).attr('idToHide')
+    $('#toggle'+idToHide).css('display', 'none')
+    $('#toggle'+idToToggle).css('display', 'block')
+  })
+
   $('.modal-toggle').mouseenter(function(){
     spe = $(this).attr('spe')
     $(this).text(spe)
@@ -123,7 +143,6 @@ $(document).ready(function(){
     name = $(this).attr('name')
     $(this).text(name)
   })
-
 
   $('.link').on('click', function(){
     containerToToggle = $(this).attr('toToggle')
